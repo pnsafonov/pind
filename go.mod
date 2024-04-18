ff3f48f30a032da48317f1034633c7b4a67ce1da
Pavel Safonov <p.safonov@postgrespro.ru>
2024-04-18 16:12:56 +0300

    test

--- go.mod
+++ go.mod
@@ -3,11 +3,15 @@ module github.com/pnsafonov/pind
 go 1.19
 
 require (
+	github.com/lrita/numa v1.0.2
+	github.com/prometheus/procfs v0.12.0
+	github.com/sirupsen/logrus v1.9.3
+	golang.org/x/sys v0.12.0
+	gopkg.in/natefinch/lumberjack.v2 v2.2.1
+	gopkg.in/yaml.v3 v3.0.1
+)
+
+require (
 	github.com/intel-go/cpuid v0.0.0-20181003105527-1a4a6f06a1c6 // indirect
-	github.com/lrita/numa v1.0.2 // indirect
-	github.com/prometheus/procfs v0.12.0 // indirect
-	github.com/sirupsen/logrus v1.9.3 // indirect
-	golang.org/x/sys v0.12.0 // indirect
-	gopkg.in/natefinch/lumberjack.v2 v2.2.1 // indirect
-	gopkg.in/yaml.v3 v3.0.1 // indirect
+	golang.org/x/sync v0.3.0 // indirect
 )
--- pkg/numa/cpu_info_test.go
+++ pkg/numa/cpu_info_test.go
@@ -1,11 +1,46 @@
 package numa
 
-import "testing"
+import (
+	"github.com/prometheus/procfs/sysfs"
+	"testing"
+)
 
-func TestCPUInfo(t *testing.T) {
+func TestCpuInfo(t *testing.T) {
 	cpuInfos, err := procfs0.CPUInfo()
 	if err != nil {
 		t.FailNow()
 	}
 	_ = cpuInfos
+}
+
+func TestCpuTopology(t *testing.T) {
+	fs0, err := sysfs.NewDefaultFS()
+	if err != nil {
+		t.FailNow()
+	}
+
+	cpus, err := fs0.CPUs()
+	if err != nil {
+		t.FailNow()
+	}
+
+	l0 := len(cpus)
+	for i := 0; i < l0; i++ {
+		cpu0 := cpus[i]
+		topology, err := cpu0.Topology()
+		if err != nil {
+			t.FailNow()
+		}
+		_ = topology
+
+		number := cpu0.Number()
+		_ = number
+
+		throttle, err := cpu0.ThermalThrottle()
+		if err != nil {
+			//t.FailNow()
+			t.Logf("cpu0.ThermalThrottle err = %v\n", err)
+		}
+		_ = throttle
+	}
 }
